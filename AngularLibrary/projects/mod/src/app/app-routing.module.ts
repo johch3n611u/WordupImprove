4bd411ae61c8937f33a529a50ba40005b0a6fdcd
DESKTOP-9SELDLU\user <sjmjgd1165@gmail.com>
2023-07-19 00:09:36 +0800

    dynamic-masonry

--- AngularLibrary/projects/mod/src/app/app-routing.module.ts
+++ AngularLibrary/projects/mod/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SampleComponent } from './components/sample/sample.component';
+import { DynamicMasonryComponent } from './layout/dynamic-masonry/dynamic-masonry.component';
 
 const routes: Routes = [
-  { path: 'sample', component: SampleComponent , pathMatch: 'full' },
+  { path: '', component: DynamicMasonryComponent },
+  { path: 'sample', component: SampleComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
--- AngularLibrary/projects/mod/src/app/layout/dynamic-masonry/dynamic-masonry.component.ts
+++ AngularLibrary/projects/mod/src/app/layout/dynamic-masonry/dynamic-masonry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-masonry',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class DynamicMasonryComponent {
 
+  @ViewChild('columns') columnsEle: ElementRef = {} as ElementRef;
+  @ViewChild('flex') flexEle: ElementRef = {} as ElementRef;
+  @ViewChild('grid') gridEle: ElementRef = {} as ElementRef;
+
+  template: any;
+  masonry = '';
+  brick = '';
+  items = [
+    { color: '' }, { color: '' }, { color: '' }, { color: '' }, { color: '' }, { color: '' }, { color: '' }, { color: '' }, { color: '' }
+  ];
+
+  ngOnInit(): void {
+    this.items.forEach(item => item.color = this.GetRandomColor());
+  }
+
+  GetRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "background-color:#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color + ';';
+  }
 }
