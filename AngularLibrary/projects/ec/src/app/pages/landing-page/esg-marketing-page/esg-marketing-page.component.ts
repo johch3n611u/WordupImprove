242866928aa0badffee37347d7e9d5fc4fea271a
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2023-07-24 23:49:29 +0800

    esg-landingpage

--- AngularLibrary/projects/ec/src/app/pages/landing-page/esg-marketing-page/esg-marketing-page.component.ts
+++ AngularLibrary/projects/ec/src/app/pages/landing-page/esg-marketing-page/esg-marketing-page.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class EsgMarketingPageComponent {
 
+  cards = [
+    { title: '兩斤勘吉', text: '1% of profits are donated to Rainforest Rescue to preserve and protect endangered rainforests.' },
+    { title: '秋本麗子', text: '4% of profits to charities and social enterprises to support positive change in the community.' },
+    { title: '野比大雄', text: 'Bringing customers, industry, government and the public together to champion composting.' }
+  ];
 }
