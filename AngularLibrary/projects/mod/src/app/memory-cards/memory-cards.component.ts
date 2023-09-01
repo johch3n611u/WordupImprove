f923900b03e32c287cff63a81d8d87f068148b15
DESKTOP-9SELDLU\user <sjmjgd1165@gmail.com>
2023-09-02 01:46:51 +0800

    memory cards

--- AngularLibrary/projects/mod/src/app/memory-cards/memory-cards.component.ts
+++ AngularLibrary/projects/mod/src/app/memory-cards/memory-cards.component.ts
@@ -7,9 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./memory-cards.component.scss']
 })
 export class MemoryCardsComponent {
+
+  nowQuestion: any = {};
+  display: any = '';
+  filterActive = 'all';
+  btnWord = '答案';
+
+  object = {
+    titles: [] as string[],
+    types: [] as string[],
+    questions: [] as object[]
+  };
+
+  url = 'https://sheets.googleapis.com/v4/spreadsheets/1KelsVdzmFwaW7zZtMzAgYX4YflhhBhC71O0nS995CDY/values/MemoryCards?key=AIzaSyA9Rj7WbM2Zsq-Yy2heVkcalnuX8Z98oOg'
+
   constructor(private httpClient: HttpClient) {
-    httpClient.get('https://docs.google.com/spreadsheets/d/1KelsVdzmFwaW7zZtMzAgYX4YflhhBhC71O0nS995CDY/gviz/tq').subscribe(data => {
-      console.log(data);
-    })
+    this.httpClient.get(this.url)
+      .subscribe((res: any) => {
+        this.object.titles = [...res.values[0]];
+        this.object.types = [...res.values.slice(1).map((item: any) => item[0])];
+        this.object.questions = [...res.values.slice(1).map((item: any) => {
+          return {
+            t: item[0], q: item[1], a: item[2]
+          }
+        })];
+        this.drawCard();
+      });
+  }
+
+  getRandomIndex(max: number, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  drawCard() {
+    let nowCard = this.object.questions;
+    if (this.filterActive != 'all') {
+      nowCard = [...this.object.questions.filter((res: any) => res.t == this.filterActive)];
+    }
+    this.nowQuestion = nowCard[this.getRandomIndex(nowCard.length - 1)];
+    this.display = this.nowQuestion.q;
+    this.btnWord = '答案';
+  }
+
+  seeAnswer() {
+    this.display = this.nowQuestion.a;
+    this.btnWord = '抽卡';
+  }
+
+  clickBtn() {
+    if (this.btnWord == '抽卡') {
+      this.drawCard();
+    } else {
+      this.seeAnswer();
+    }
   }
 }
