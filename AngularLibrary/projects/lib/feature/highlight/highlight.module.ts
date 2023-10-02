81b35b213d8c663802e1c0c3add99d5d19c39e30
DESKTOP-9SELDLU\user <sjmjgd1165@gmail.com>
2023-10-02 11:49:18 +0800

    highlight pipe

---
+++ AngularLibrary/projects/lib/feature/highlight/highlight.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HighlightPipe } from './highlight.pipe';
+
+@NgModule({
+  declarations: [HighlightPipe],
+  imports: [CommonModule],
+  exports: [HighlightPipe],
+  providers: [],
+})
+export class HighlightPipeModule {}
