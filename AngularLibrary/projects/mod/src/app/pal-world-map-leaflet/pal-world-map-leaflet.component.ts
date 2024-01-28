fc687ebc7462c61c586bb040c981acdff2dd641d
yuchang liu <allen@webglsoft.com>
2024-01-28 12:57:21 +0800

    new assets

--- AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
+++ AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
@@ -4,47 +4,52 @@ import * as L from 'leaflet';
 @Component({
   selector: 'mod-pal-world-map-leaflet',
   templateUrl: `./pal-world-map-leaflet.component.html`,
-  styleUrls: ['./pal-world-map-leaflet.component.scss']
+  styleUrls: ['./pal-world-map-leaflet.component.scss'],
 })
 export class PalWorldMapLeafletComponent implements AfterViewInit {
   private map: any;
   markers: L.Marker[] = [
     L.marker([31.9539, 35.9106]), // Amman
-    L.marker([32.5568, 35.8469]) // Irbid
+    L.marker([32.5568, 35.8469]), // Irbid
   ];
   private addMarkers() {
     // Add your markers to the map
-    this.markers.forEach(marker => marker.addTo(this.map));
+    this.markers.forEach((marker) => marker.addTo(this.map));
   }
   private centerMap() {
     // Create a LatLngBounds object to encompass all the marker locations
-    const bounds = L.latLngBounds(this.markers.map(marker => marker.getLatLng()));
+    const bounds = L.latLngBounds(
+      this.markers.map((marker) => marker.getLatLng())
+    );
 
     // Fit the map view to the bounds
     this.map.fitBounds(bounds);
   }
 
-
   //
   private initRealMap(): void {
-    const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     this.map = L.map('map');
     L.tileLayer(baseMapURl).addTo(this.map);
   }
 
   private initPalWorldMap(): void {
     const map = L.map('map', {
-      crs: L.CRS.Simple
+      crs: L.CRS.Simple,
     });
 
     let corner1 = L.latLng(0, 0);
     var corner2 = L.latLng(1000, 1000);
     let bounds = L.latLngBounds(corner1, corner2);
-    let image = L.imageOverlay('./assets/palpagos_islands.webp', bounds).addTo(map);
+    let image = L.imageOverlay('./assets/palworld/palpagos_islands.webp', bounds).addTo(
+      map
+    );
     map.fitBounds(bounds);
   }
+
   ngAfterViewInit(): void {
     this.initPalWorldMap();
   }
+
   // https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet
 }
