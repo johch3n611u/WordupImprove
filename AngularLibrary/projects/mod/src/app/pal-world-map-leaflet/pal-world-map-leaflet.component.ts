90bbc6ae14dbf66c12e5d175aa422d87d1d230c6
yuchang liu <allen@webglsoft.com>
2024-01-28 21:09:52 +0800

    no message

--- AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
+++ AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
@@ -1,6 +1,7 @@
 // https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet
 // https://dowyuu.github.io/program/2021/03/22/Leaflet-note/
 // https://gamewith.net/palworld/43313
+// https://jsfiddle.net/newluck77/rk9v0uyo/
 
 import { AfterViewInit, Component, HostListener } from '@angular/core';
 import * as L from 'leaflet';
