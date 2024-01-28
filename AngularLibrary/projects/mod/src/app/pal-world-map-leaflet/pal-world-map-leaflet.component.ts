3f8e6aa6e3fa54d780c2c45931e32596d34e3bdf
yuchang liu <allen@webglsoft.com>
2024-01-28 16:54:43 +0800

    衝突

--- AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
+++ AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
@@ -1,3 +1,7 @@
+// https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet
+// https://dowyuu.github.io/program/2021/03/22/Leaflet-note/
+// https://gamewith.net/palworld/43313
+
 import { AfterViewInit, Component, HostListener } from '@angular/core';
 import * as L from 'leaflet';
 
@@ -10,10 +14,18 @@ export class PalWorldMapLeafletComponent implements AfterViewInit {
   private map: any;
   markers: L.Marker<L.LatLng>[] = [];
 
-  public addMarkers() {
-    // Add your markers to the map
-    this.markers.forEach((marker, index) => marker.addTo(this.map).on('click', (e) => { e.target.remove(); this.markers.splice(index,1)}));
-  }
+  // private initRealMap(): void {
+  //   const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  //   this.map = L.map('map');
+  //   L.tileLayer(baseMapURl).addTo(this.map);
+  // }
+
+  // @HostListener('mousemove') onMouseMove(e: any)
+  // {
+  //   console.log(e);
+  //   let latLng = e.latlng;
+  // }
+
   private centerMap() {
     // Create a LatLngBounds object to encompass all the marker locations
     const bounds = L.latLngBounds(
@@ -24,31 +36,27 @@ export class PalWorldMapLeafletComponent implements AfterViewInit {
     this.map.fitBounds(bounds);
   }
 
-  //
-  // private initRealMap(): void {
-  //   const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-  //   this.map = L.map('map');
-  //   L.tileLayer(baseMapURl).addTo(this.map);
-  // }
+  bossesMarkersLayer = L.layerGroup();
 
-  markerAry = <any>[];
   currentLat = 0;
   currentLng = 0;
-  private onMousemove() {
-    this.map.on('click', (e: L.LeafletMouseEvent) => {
-      console.log(e.latlng);
-      this.currentLat = e.latlng.lat;
-      this.currentLng = e.latlng.lng;
-      const marker = L.marker([e.latlng.lat, e.latlng.lng])
-      this.markers.push(marker);
+  private onMouseClick() {
+    this.map.on('click', (mapClick: L.LeafletMouseEvent) => {
+      this.currentLat = mapClick.latlng.lat;
+      this.currentLng = mapClick.latlng.lng;
+      let marker = L.marker([
+        mapClick.latlng.lat, 
+        mapClick.latlng.lng
+      ]).on('click', (markerClick) => {
+        markerClick.target.remove();
+      });
+
+      this.bossesMarkersLayer.addLayer(marker).addTo(this.map);
+
+      console.log(this.map)
     });
   }
 
-  // @HostListener('mousemove') onMouseMove(e: any)
-  // {
-  //   console.log(e);
-  //   let latLng = e.latlng;
-  // }
   private initPalWorldMap(): void {
     this.map = L.map('map', {
       crs: L.CRS.Simple,
@@ -57,17 +65,15 @@ export class PalWorldMapLeafletComponent implements AfterViewInit {
     let corner1 = L.latLng(0, 0);
     var corner2 = L.latLng(1000, 1000);
     let bounds = L.latLngBounds(corner1, corner2);
-    let image = L.imageOverlay('./assets/palworld/palpagos_islands.webp', bounds).addTo(
-      this.map
-    );
+    let image = L.imageOverlay(
+      './assets/palworld/palpagos_islands.webp',
+      bounds
+    ).addTo(this.map);
     this.map.fitBounds(bounds);
   }
 
   ngAfterViewInit(): void {
     this.initPalWorldMap();
-    console.log(this.map);
-    this.onMousemove();
+    this.onMouseClick();
   }
-
-  // https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet
 }
