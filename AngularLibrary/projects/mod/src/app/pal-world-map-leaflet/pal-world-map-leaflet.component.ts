0098b6ab6d7a0c7397e4d1a6ff6c149045da3ee6
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2024-01-28 15:23:18 +0800

    feat: add markers

--- AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
+++ AngularLibrary/projects/mod/src/app/pal-world-map-leaflet/pal-world-map-leaflet.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -8,13 +8,11 @@ import * as L from 'leaflet';
 })
 export class PalWorldMapLeafletComponent implements AfterViewInit {
   private map: any;
-  markers: L.Marker[] = [
-    L.marker([31.9539, 35.9106]), // Amman
-    L.marker([32.5568, 35.8469]) // Irbid
-  ];
-  private addMarkers() {
+  markers: L.Marker<L.LatLng>[] = [];
+
+  public addMarkers() {
     // Add your markers to the map
-    this.markers.forEach(marker => marker.addTo(this.map));
+    this.markers.forEach((marker,index) => marker.addTo(this.map).on('click',e => e.target.remove(()=> {this.markers.splice(index,1)})));
   }
   private centerMap() {
     // Create a LatLngBounds object to encompass all the marker locations
@@ -26,25 +24,45 @@ export class PalWorldMapLeafletComponent implements AfterViewInit {
 
 
   //
-  private initRealMap(): void {
-    const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    this.map = L.map('map');
-    L.tileLayer(baseMapURl).addTo(this.map);
+  // private initRealMap(): void {
+  //   const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  //   this.map = L.map('map');
+  //   L.tileLayer(baseMapURl).addTo(this.map);
+  // }
+
+  markerAry = <any>[];
+  currentLat = 0;
+  currentLng = 0;
+  private onMousemove(){
+    this.map.on('click', (e: L.LeafletMouseEvent) => {
+      console.log(e.latlng);
+      this.currentLat = e.latlng.lat;
+      this.currentLng = e.latlng.lng;
+      const marker = L.marker([e.latlng.lat,e.latlng.lng])
+      this.markers.push(marker);
+    });
   }
 
+  // @HostListener('mousemove') onMouseMove(e: any)
+  // {
+  //   console.log(e);
+  //   let latLng = e.latlng;
+  // }
   private initPalWorldMap(): void {
-    const map = L.map('map', {
+     this.map = L.map('map', {
       crs: L.CRS.Simple
     });
 
     let corner1 = L.latLng(0, 0);
     var corner2 = L.latLng(1000, 1000);
     let bounds = L.latLngBounds(corner1, corner2);
-    let image = L.imageOverlay('./assets/palpagos_islands.webp', bounds).addTo(map);
-    map.fitBounds(bounds);
+    let image = L.imageOverlay('./assets/palpagos_islands.webp', bounds).addTo(this.map);
+    this.map.fitBounds(bounds);
   }
   ngAfterViewInit(): void {
     this.initPalWorldMap();
+    console.log(this.map);
+    this.onMousemove();
   }
   // https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet
 }
